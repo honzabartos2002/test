16664ef98d828fc429bfc6487b43d893f6846924
Jan Bartoš <jan.bartos@linksoft.cz>
2025-07-13 11:16:41 +0200

    test commitu

--- src/Screens/Login.tsx
+++ src/Screens/Login.tsx
@@ -14,7 +14,7 @@ export default function Login(props: ILoginProps) {
                 variant='contained'
                 color='primary'
             >
-                Přihlásit se
+                Přihlásit s
             </Button>
         </Box>
     );
